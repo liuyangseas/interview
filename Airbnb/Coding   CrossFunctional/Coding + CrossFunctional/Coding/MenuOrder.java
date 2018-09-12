707515eff17906a95e03d0756a35b29fbe1d2431
zmyvszk <zhoumingyang34@gmail.com>
2018-09-11 23:56:45 -0700

    add

---
+++ Airbnb/Coding   CrossFunctional/Coding + CrossFunctional/Coding/MenuOrder.java
@@ -0,0 +1,66 @@
+/**
+ * Given a menu of dishes with prices on it, you have certain amount of money. You want to use all
+ * of the money to buy different dishes. What are the combinations you could have?
+ *
+ * 本质是combination sum，但要注意价格为double，不能直接用 == 比较
+ * 1. 转成cents，即全部乘以100，这样可以作为int处理
+ * 2. 设定精度epsilon，两个数相差小于epsilon就认为是相等
+ * 也是面经的题，做了一点小变动，dishes是按照菜名+price的map给的, 面试的三哥说price没有重复不用排序，一道菜可以重复点，要输出所有的菜名combination。
+
+实际上就是combination sum 加上一点点从map输出到不同array处理的过程
+ */
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MenuOrder {
+  public List<List<Double>> getCombos(double[] prices, double target) {
+    List<List<Double>> res = new ArrayList<>();
+    if (prices == null || prices.length == 0 || target <= 0) {
+      return res;
+    }
+
+    int centsTarget = (int)Math.round(target * 100);
+    Arrays.sort(prices);
+    int[] centsPrices = new int[prices.length];
+    for (int i = 0; i < prices.length; i++) {
+      centsPrices[i] = (int)Math.round(prices[i] * 100);
+    }
+
+    search(res, centsPrices, 0, centsTarget, new ArrayList<>(), prices);
+    return res;
+  }
+
+  private void search(List<List<Double>> res, int[] centsPrices, int start, int centsTarget, List<Double> curCombo, double[] prices) {
+    if (centsTarget == 0) {
+      res.add(new ArrayList<>(curCombo));
+      return;
+    }
+
+    for (int i = start; i < centsPrices.length; i++) {
+      if (i > start && centsPrices[i] == centsPrices[i - 1]) {
+        continue;
+      }
+	  
+      if (centsPrices[i] > centsTarget) {
+        break;
+      }
+	  
+      curCombo.add(prices[i]);
+      search(res, centsPrices, i + 1, centsTarget - centsPrices[i], curCombo, prices);
+      curCombo.remove(curCombo.size() - 1);
+    }
+  }
+}
+
+//class Main {
+//  public static void main(String[] args) {
+//    MenuOrder mo = new MenuOrder();
+//    double[] prices = { 10.02, 1.11, 2.22, 3.01, 4.02, 2.00, 5.03 };
+//    List<List<Double>> combos = mo.getCombos(prices, 7.03);
+//    for (List<Double> combo : combos) {
+//      System.out.println(combo);
+//    }
+//  }
+//}
