138795f07e778e13b29c39e8229b7f042bfce23d
zmyvszk <zhoumingyang34@gmail.com>
2018-09-25 14:11:17 -0700

    add

---
+++ Amazon/Onsite/Onsite.java
@@ -0,0 +1,11 @@
+1. design Amazon Locker OODesign
+2. bar raiser -> Topoligical sort: project and completion time, projects have dependencies relationship
+3. System design: item details page with recomendation combinations
+4. Integer to Roman / Roman to Integer
+5. Behavior
+
+1. Parking lot coding, count maximum number of cars in the parking lot
+2. BFS shortest path
+3. Behavior
+4. coding a weird stuff
+5. bar raiser -> N-ary tree for LCA
