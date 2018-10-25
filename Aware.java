f86e008c11c846c8c25b1a015aadcc8ac97102d9
zmyvszk <zhoumingyang34@gmail.com>
2018-10-24 17:13:03 -0700

    Create Aware.java

---
+++ Aware.java
@@ -0,0 +1,33 @@
+下周一的下午一点：
+
+Aware hiring manager 开始时间 asap
+
+三个team manager  algorithm + SDK +
+
+第一轮：介绍，为什么转专业，实习，详细architecture，你的responsibilities，data mining的framework，语言，工具。。。
+
+题目： 设计一个餐厅预定系统  Amazon OODesign 
+
+你觉得这个系统的Database的table如何设计
+
+项目 SOA  JWT 什么是多态 design pattern了解了多少？
+
+第二轮： 纯算法
+
+OA题目： 走迷宫  BFS  + DFS
+
+A->B->c
+
+逻辑题目： 音乐会的演奏家有ABC，五个人演奏三个人的乐曲，给出一些限制条件 聪明
+
+Implement a sorting question
+
+问问题：daily life？ 具体的technology？
+
+第三轮： SDK
+
+纯聊天。。。聊项目
+
+SQL vs NoSQL SOAP vs REST
+
+
