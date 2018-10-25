bf139ec19709f31d43f15a3b219717232aa4a8b8
zmyvszk <zhoumingyang34@gmail.com>
2018-10-24 17:07:11 -0700

    Create OA.java

---
+++ Amazon/Onsite/OA.java
@@ -0,0 +1,286 @@
+
+技巧总结
+
+
+有关字母跟数字的:
+
+类比类:3组英文字母,给你推空着的那组应该是什么。 4选1,不难但是花时间,要注意时间不要用太多了。提前在纸上把26个字母跟对应的数字写上,可以节省很多时间。
+排异类:给你4组英文字母,叫你找出pattern不同的那组,基本就是同上。
+数列类:一串数列,叫你推最后一个。地里面经看过应该就知道大概要怎么去想了。
+转译类:xxxxx转成ooooo,问你aaaaa会转成什么这种,多半是奇偶位交错+x -x。
+知道规律后可以看看答案，有的选项有明显错误可以直接排除
+
+规律题
+QPS : TSV -> IHK : (LKN)   都是+3
+DJ:WQ :: FK: ? (UP) 选项：UP，UR, RU, PU (UP吧。因为DJ+6, WQ-6. FK+5, 下一个数字应该是减5)
+46 : 64 -> 82 : (100)  （差为18且后一个比前一个大）
+EAGLE : FZHKF -> THANKS : (UGBMLR)  +1, -1 找规律的（奇数+1偶数-1）
+FASTER : HCUVGT -> SLOWER --> (UNQYGT) （+2）
+985 : 874 -> 763 : (652) （每一位减一即可）
+865 : 532 -> 976 : (643) （右边是左边每一位减三）
+ADBC : EHFG -> ILJK : (MPNO)（4个一组）
+JOHN : LSNV -> MARK : (OEXS)（+2 +4 +6 +8）
+COMPUTER : PMOCRETU -> TELEVISION : (VELETNOISI)（镜像倒过来，从中间分开）
+A17R : D12P -> G7N : J2L(?) （R=A+17, D和A差3，12和17差5）
+VALIANT:UBKJZOS::TRANSCEND: (SSZORDDOC) +1-1
+VAILANT : UBKJZOS -> TRANSCEND : SSCLRDGLC (NOT SURE 奇数对：奇数-1，偶数+1，偶数对：奇数-2，偶数+2)
+COMPUTER : GKQLYPIN  -> SENATE : WARWXA（奇数+4偶数-4）
+KPQR : LRTV -> DGHY : (EIKC) (前后相减每一位的增加分别为1,2,3,4)
+STICK -> RSHBK    REPLY->? （QDOKX, 前后每个字母差-1？最后一个一样？）
+EAGLE -> FZHKF    THANKS-> ? (UGNLMR, +1,-1,+1,-1,+1)
+ACFJ : CEHL -> PRUY : (RTWA) (前后相减每一位的增加分别为1,2,3,4)
+27 : 24 -> 64 : (60)   (3^3 - 3 = 24 4^3 - 4 = 60)
+MQD : KRK -> SWM : ( QXT) (13, 17, 4, 11, 18, 11 => 13 - 18 = -5, 17 - 11 = 6, 4 - 11 = -7；5,6,7连续,正负号交替；另一个answer是NCF) 注意最后的位置
+AD5 : ED9 （A + D = 5, E + D = 9）
+选C (规律： +1-1+1-1+1-1+1)
+
+
+排异题
+BGL : DIN : MRW : (HLR) （排异题，前三项都是差5，最后一个差6：HLR:8 12 18）
+PRS TVX FIK (LME)（前三项均为递增）􀴭􀭑 16 18 19 20 22 24 6 9 11 12 13 5
+JLP（LNT）TVZ DFJ（排异题，剩余三项都是差2差4，第二个差2差6）
+(ABIJ) : DEHI: MNQR: STWX（排异题，剩余三项都是1和三之间差4，第一个差8）
+(RHCAI) OEST HNDA ADEH （只有这个不是身体部位,toes, hand, head）
+ADP QTS HKR STE （不确定 ADP: 所有对应index均为平方数，另外三项不具备；QTS：ADP = 1， 4， 16 = 00001， 00100， 10000， 位与的结果是0
+       QTS = 17，20，19 = 10001， 10100， 10011，位与的结果是10000， HKR = 8，11，18 = 01000，01011，10010，位与的结果是0， STE = 19，20，5 = 10011，10100，00101，位与的结果是0）
+ADF (MPR) ILN EHJ (MPR不是以元音开头？)
+STV XYA （KKT） BDE
+
+STV
+XYA
+KKT
+BDE
+19 20 22
+24 25 1/27
+11 11 20
+2 4 5
+间隔 1, 2
+1，2
+0，9
+2，1
+
+
+异数
+
+956 794 884 (678）(前面三个加起来都是20，最后一个加起来是21)
+1,4,16；17,20,24；8,11,18；（19,20,5）（前面三个均是递增，最后一个不是）
+AE5 DF6 HN14 （KP2）（E=5,F=6,N=14,P!=2）
+（HIK） DGJ LPT SUW（其他都是等间距）
+HIK
+DGJ
+LPT
+ SUW
+8 9 11
+4 7 10
+12 16 20
+19 21 23
+间隔 1, 2
+3，3
+4，4
+2，2
+异数
+
+
+
+LKJI (XYWV) WVUT KJIH (其他三个都是相邻倒序)
+填空
+2,3,7,8,13,14,(20)（间隔规律为1,4,1,5,1,6）
+8，8，15，23，38，？选项是61，57，62，59准备的时候特意研究了这个题，大家都选61……一脸茫然的跟选了(楼主觉得应该是两数间间距为前面两个间距的和)
+27:24 ：：64：？（60， 3*3*3: 2*3*4 ::. 4*4*4: 3*4*5）
+0 1 1 2 4 8 (16) （16是前面的和）
+3,6,18,108,(1944) （3x6=18, 6x18=108, 18x108 = 1944）
+1,1,4,2,13,3,40,4,( 121 ) (1+3的1次方=4，4+3的2次方=13，13+3的3次方=40，40+3的4次方=121)
+3 7 13 21 (31) (间隔以4，6，8，10递增)
+3 11 25 45 （71）(间隔为8，14，20，26)
+5 11 19 29 (41) (间隔以6，8，10，12递增)
+0 2 6 12 20 (30) （间隔以2，4，6，8，10递增）
+5 9 16 29 ( 54 ) （5*2-1, 9*2-2, 16*2-3, 29*2-4）51是不是也可以呢？
+4 12 6 18 12 36 30 ( 90) （没两个一组，后一个是前一个x3）
+1 5 7 (8) (1+2^2 = 5 5+2^1 = 7 7+2^0 = 8)
+D, H, L, (p) (间隔为4)
+10 14 23 39 64 (100) （间距为完全平方数）
+10 74 202 394 (650)（间距递增64, 64x2,64x3,64x4）
+2 8 5 6 8 (4 ) 11 （两两这么看的， 2、5、8、11， 加3得来的。 另外一组应该是减2，也就是8、6、4）
+16 30 46 62 (78) （30 + 16, 46 + 16, 62 + 16）
+1:4:27:256:( 3125 ) （N的N次方）
+2, 5, 26，(677)（规律是当前数字是前一个数字平方加1）
+ASSERTIVENESS-> SENSSAEVISTRE : MULTINATIONAL ->(ANOLUMITALNIT) ( 位置mapping )
+4，5，12，8，9 => 3，4，1，7，8 : 13, 21, 13, 2, 1, 9 => ? ( 4, 5, 12, 8, 9 => (4 - 1), (5 - 1), (1 - 0)(2 - 1), (8 - 1), (9 - 1) => 13 21 13 2 1 9 ->(1-1)(3-1) (2-1)(1-1) (1-1)(3-1) 1 26 8 就是2 10 2 1 26 8 （B J B A Z H))
+0 1 1 2 3 7 (22 ) ( 0 * 1 + 1 = 1; 1 * 1 + 1 = 2; 1 * 2 + 1 = 3; 2 * 3 + 1 = 7; 3*7 + 1 = 22 )
+1 1 3 5 8 13 （21）斐波那契数列
+应用题
+if northwest becomes east, northeast becomes south, and so on, what does southeast become? (west) 
+Lily can't find her home, she is 25 yards southwest of her home, then she walked 20 yards toward north, where is her home from her now?
+一个面朝北的朋友，先左走15m，然后一个about-turn走了30，这货在哪？(about-turn:  a turn made so as to face the opposite direction, so he is 15m east)
+东2北8再南2，选northeast
+另外一个美女，向东走了4，向北走了8，向西走了2，现在跟之前的出发点相比，在哪个方向(NE)
+一个人往东2，再往南3，再往西2，再往北2，选 他现在在他原位置的南边 
+小明往东南走4 miles，往西走8 miles， 再往西北走4 miles。现在小明离出发点是什么方位？（正西(W), 平行四边形）
+小明面朝南，往左走20miles， 再往右走 10miles， 再往左走30miles。 现在小明离出发点是什么方位？(大致东南方向)
+小明从原点出发往北走了一段距离，左转走了10再左转走了20，发现自己在出发点西侧10，问一开始往北走了多少？（20）
+南5西4南7东4北5，问方向、离原点距离(在原点正南7处)
+一个楼有3层，每个level 坐一些人，第二层能坐最多，一共坐66个人。给了两个条件： 1，其中有一层坐了21 人。 2，第二层比其中一层多座了2人，求第二层坐了多少人（23，第一层21第三层22）
+推断一个人的年龄 （1）知道所有人的平均年龄 （2）所有人年龄都一样，问（1）和（2）怎么来推断这个人的年龄 ?????
+妹妹比哥哥小5岁（或其他数字）， 哥哥1988年，判断能计算什么(妹妹的年龄，妹妹的出生年)
+排名问题：给两个条件： I. 班级总人数38， II 比小王分数低的有19人
+选 （结合I,II两个条件可以决定小王在班级的排名）
+买车问题：给两个条件: I. 小王买车时间 <10/19 且 >=10/16 
+II.小王买车时间 >10/17 且 <=10/20
+选 （结合I,II 两个条件可以确定小王具体买车时间.） 
+下一道是推买车日期 两个条件加一起可以推出她是哪天买车的
+a better sales person would be the one who is able to explain the features of his/her product in a simple manner
+是桌上一堆球，取掉7个不少于23个， 加上6个不多于20个，问能否推断桌上几个球 (不能，不成立)
+说有M1,M2,M3,M4,M5和W1,W2,W3。出差必须派至少三男一女。M1和M3不能共存，M4和W2不能共存。
+第一问问如果派了M2和M3和W2，还可以派谁 （至少三男一女则M1和M4都不可能，所以M5, W1,W3）
+第二问问如果M1 M2去了，还可以派谁。（M5W2W1W3,M4W1W3,M5M4W1W3）
+第三问如果去了四个男生，那么谁不能选。(W2,因为M1和M3不能共存，则M4一定在，则W2不能在)
+生日在9月19~25， leap year，判断能否能计算出生日 ????? (29, common year 28)
+A-B表示A加B，A#B表示A乘B，A/B表示A大于等于B，A？B表示A小于B。 给出条件： (V # X) / (V – X), X ? Y and Z/Y 翻译过来即V*X >= V+X, X < Y, Z >= Y 问以下两个结论哪个正确 1. X < Z(x<Y<=Z) 2. V+X < V*X(v+x也可能=V*X, 所以选1)
+直接转前辈的面经：印度公司在radio上做广告，记忆没错的话，选B。大意是，radio覆盖面广，公司向推广自己的，应该去上面做广告。It has been provenby research that in India, a company which purchases saturation radioadvertising will get maximum brand recognition. a) A highdegree of brand recognition will help a company win a higher share of themarket. b) Radio has wide listenership and companies intending to increase their awareness,should advertise it. c) Formaximum brand recognition, a company need not spend on media channels otherthan radio publicizing. d) Brandrecognition in India is more heavily dependent on where the brand advertisesthan the quality of its offering. 原题说的saturation advertising是指同一个广告反复宣传，就好像脑白金的洗脑歌一样。。而研究表明通过这种广播，可以收获最大的品牌认同度。 A.高品牌认同度使公司获得更高的市场份额（研究说的是宣传与认同度的关系，没有涉及市场份额）（品牌认知度和市场份额的关系未在题目条件中提及，无关联） B.广播有不错的听众基础，如果公司想提高他们的知名度认同度，应该考虑通过这种方式. C.为了达到最大的品牌宣传效果，公司不应该考虑广播之外的宣传方式 （广播频率max导致宣传效果max也只是广播频率的影响，并不代表其他宣传渠道如何）。（并未说明其他广告渠道对最大化品牌认知度的贡献，所以不该武断排除其他一切非收音机渠道的广告策略） D. 在印度，品牌认同更看宣传的where，而不是质量 （同样原文没提到质量和效果之间的关系）（题目条件未提及广告质量，无关联）
+问想要求今年有多少个冰箱sold了，需要下述哪几个条件 1. 今年售出的冰箱是去年的3倍 2. 去年sold了40个冰箱 我的答案是两个条件都需要
+第一道是老师忘带课表 然后有四节课 每节一小时 给两个条件 感觉应该是加一起可以推出课表
+在以下4种元素中（锂，钠，钾，铯）L能量随着原子的半径增加而减少，想要求那个L能量最小，需要下述哪几个条件 1. 锂的原子半径最小 2. 铯比锂和钾的半径都大 我的答案是所有条件都不能确定
+题是现在员工都喜欢查看和及时回复email, balabala，说是上班时候也喜欢是不是看手机，这种行为影响了deadline, process啥的， 我选的选项是email has an adverse（逆向的，有害的） affect on performance
+选择是否将候选公司放到一个环保list上:
+条件是： 1 --> hava ECC (一种认证).
+              2 -->生成了至少三种solar 产品
+              3-> none of their products are from synthetic ...(这个词我记不清了)
+              4-->headquater in Texas
+              5 -->product 都由 A -certificate
+              6 -->donot have legal dispute or pending against them
+如果不满足 2,但是有一种产品正在试验中 -->推荐给COO
+如果不满足5 --> 推荐给Director of the company
+第一个公司，只有两个产品，没说有没有第三个产品。其他条件都满足。要不要选？现在想想应该选insuffient information的（报告COO?）
+第二个公司，这6个条件都满足，可以进入会议
+          
+一个公司要招PM，合理的candidate需满足以下条件： 
+本科是学CS的
+有MBA学位
+本科GPA 3.0+
+如果没有MBA学位，但是工作5年以上，需上报HR
+本科不是学CS，但是在CS相关工作3年以上，上报HR
+那么，请问：闰土本科学热水锅炉维修的，GPA 4.0，没有念过MBA，在Google修了5年的锅炉，当
+了3年的程序员，则应该： D
+
+A. 录用
+B. 不录用
+C. 条件不充分
+D. 上报HR
+
+条件是：必须有硕士学位，且GPA为A
+2.必须有3年以上工作经验
+3.若1不满足报告director
+小明从事某工作三年，有CS和MBA，本科GPA为A-则：3.上报director
+有某个岗位，要想上岗，得具备以下条件:
+1. 要有Master of Commerce, 并且GPA>=B, 或者有CPA
+2. 到2014年12月1日，年龄应该是20-25岁. 
+3. 熟练说English and Spanish
+4. Pay $125 deposit
+5. Make a commitment to work >= 5 years
+如果1不满足，就上报manager director
+如果4不满足，就上报chairman
+问题：
+第一个candidate, 4不满足，于是需要上报chairman
+第二个candidate, 1994年11月1日出生，其他条件都满足，于是这个小伙子就入选了。
+快递费要不要收的问题。条件是
+地区code 大于10一类，小于10 另一类
+商品价格超过500
+不是deal的时候买得
+之前没有bulk 超过5%的折扣. 
+客户有优良购买记录3年
+
+如果不满足2，那么要是他满足地区code小于10，收10刀，大于10，收8刀。
+如果不满足3，那么region code小于10，收5刀，大于10，收12刀
+来了一个老头，买了150刀的东西（2不满足），不是deal的时候买的，也没有之前折扣（5也不满足）。问他可不可以不付运费。
+若不满足两条，则必须付全款。（选：附全款30刀）
+第二个case, Emma, 从ABC买了4年，没有bulk discount, area code 08, 上个月买了150的东西，有2%special discount, 有dealership, 加粗的不满足，也交30
+石油公司招聘
+
+
+       
+There are four coordinators named Lily, Cathy,Mary and Nina. Each coordinator is at a different corner of the rectangle meeting hall. A coffee vending machine is situated at one of the corners and a restroom at another corner of the meeting hall. Lily and Cathy are at either sides of the white board, which is situated at the center of the side  which is opposite to the side at whose corners the coffee vending machine and the restroom are located. Coordinator Mary is not at the corner where the restroom is located. 
+Which of the following cannot be true?
+Lily is not on the side of the hall where the white board is placed
+Nina is adjacent to the restroom at one corner
+Cathy is at the corner, adjacent to the coffee vending machine.
+Mary is adjacent to the coffee vending machine, at one corner of the hall
+Lily is at the corner, adjacent to the coffee machine
+ 选1？Lily和cathy推出3,5Mary这个推出2,4（注意23和45的区别）
+重点是这句“on either sides of the white board”，俩人分别在whiteboard的一侧，而不是要在hall的white board side,我觉得可能性有很多(V是vending machine)
+_________________________
+| C                   N(R)|
+| ||                      |
+| ||(W)                   |
+| ||                      |
+| L__________________ M(V)|
+_________________________
+| N                   C(R)| 
+| |                       |
+| |(W)                    |
+| |                       |
+| L__________________ M(V)|
+等情况
+
+总之就是lc可以互换，其他两人不可。
+
+只要M不在Vending machine那边，L和C分别在white board两侧应该就可以…
+
+还有问题里这个选项：Mary is adjacent to the coffee vending machine, at one corner of the hall，Mary到底是在和vending machine相邻的corner还是就在vending machine呢？ 私以为是在vending machine的相邻的两个corner，但是这样看不出哪个选项有问题…
+
+第一个选项1. Lily is not on the side of the hall where the white board is placed 是说L不在WHITE BOARD放置的那一侧，错。
+第二个选项2. Nina is adjacent to the restroom at one corner是说N与RESTROOM相邻，在某一CORNER，对。
+第三个选项3. Cathy is at the corner, adjacent to the coffee vending machine
+是说C在某一个CORNER，那个CORNER与COFFEE VENDING MACHINE 相邻，所以不是COFFEE VENDING MACHINE的那个CORNER，对。
+第四个选项4. Mary is adjacent to the coffee vending machine, at one corner of the hall 是说M与COFFEE MACHINE 相邻，在某一个CORNER，对。
+第五个选项5. Lily is at the corner, adjacent to the coffee machine 是说L在某一个CORNER，那个CORNER与COFFEE MACHINE相邻，对.
+
+
+八产品  A manufacture company has 8 products and 4 divisions. Four divisions are lead by Alan, Betty, Cathy, Diana. The 8 products are: mixer, iron, water pump, geyser, juicer, blender, grinder, and heater. Each division produces 2 products, no 2 divisions produces the same product. Diana’s division produced Geyser, Cathy’s division produces water pump. Mixer and iron areproduced by division lead by Alan and Betty respectively. The division that produces mixer doesn’t produce blender.
+               Four questions:
+               1. if the division that produces mixer doesn’t produce juicer, which of the following statement is true?
+               4. if the division that produces mixer also produces juicer,how many ways are there for product pairs? (3! = 6 because respectively A生产M,B生产I,那M和J一起     的话则A的生产产品定，B，C, D有一个产品是定下的，所以还剩三个产品分配给三个人，so 3x2x1=6)
+               For factory problems, take care of the global assumptions andl ocal assumptions.
+          
+一圆桌坐八人ABCDEFGH. F在C右边两位,AE坐G两边(两边不一定是相邻的，可以是有间隔的), BH面对面:
+以下哪两人坐对面?D&G
+问D对面是谁?G
+谁坐D旁边?C
+AB不坐隔壁, F对面坐A, 反时针方向可能的坐法?AHCDFBEG之类的
+
+6人团团坐问题，有六个人 GASMNR, 注意理解 G, A，S 两两不能对坐，所以总体来说分两种情况，GAS三人间隔而坐，或者GAS全都挨着坐。所有团团做的问题都围绕此基础展开。1. 有一题说R 在A S 中间，问你R对面是谁(since GAS不能对坐，那么只能是G)。2. G左右是A R,问A 对面是谁（M或N都可以）
+说甲乙丙丁排排坐，甲在乙右边，丙在丁右边，能不能推断粗谁最右。（不能）
+说我生日在二月和十月之间，在四月和八月之间，（都是non-inclusive)，你猜猜我生日到底几月啊。（这什么意思。。谁遇到过可以给我解释一下不）
+ 阅读题真人秀 (答案： 参加的人通过做bizarre 的事情得到钱）
+方向题选B(3yard to east, 12 yards to south)
+
+选D(NE)
+
+41. 另一个阅读题应该是讲顾客不是很容易被操作，因为他们很清楚的知道别人跟他们想的不一样的。我选的是最后一个，好像是商店并不能get到顾客真正想要的
+42. 一个人从point 1向south走了20miles，然后turn right， 有向前走了30miles，然后第二次turn right,再向前走了20miles。问第二次turn right后朝什么方向前进。(N)
+％ means greater than; > means equal;  = means is not less than; @ means not equal; # means less than;* means no greater than.然后给了三个条件：A%B (A > B),  C=E (C >= E), D*B (D<=B)， 问你1）A#D (根据条件可推出 A >= D, A#D = A<D, 不对)    2）C*E 里面哪个是成立的（C*E=C<E, 根据条件不对），所以两个都不成立。
+有四个人R M K G(其实都有名字的，为了偷懒只记录了首字母)，哪个人最高？
+         给了两个条件：
+        1. M 比G高，但是M比R矮
+        2. K比M高
+      （这两个条件加起来也得不出来）.
+
+           
+说是有两家网站I和B，这两家都在网上卖东西。B家可以卖二手，然后大家可以去竞拍，但是出价最高的必须买下来。这家没有warranty。另外一家可以从商家直接买新的，然后问了一些问题，LZ选的是（竞拍最高价必须买下来)
+
+说有个fiction网站，是用来给科幻小说的读者来yy接下来桥段的，读者可以自己在上面续写科幻小说。比如哈利波特，如果哈利最开始没加入格兰芬多加入斯莱特林了呢？如果哈利没和好人做朋友而是成为马尔福集团的一份子了呢？etc. 那么接下来剧情咋发展呢？有的作者觉得这个网站挺好的，因为可以受到启发，或者看看读者希望剧情如何发展。有的作者觉得这网站挺破的，因为你自己写故事为毛不自己新开一个坑写呢？而且还会干扰我思路……
+               选项有类似：这个网站使得人们变得更有想象力了。(这个网站对author有cons也有pros)。(http://www.1point3acres.com/bbs/forum.php?mod=viewthread&tid=203975&extra=page%3D5%26filter%3Dsortid%26sortid%3D311%26searchoption%5B3086%5D%5Bvalue%5D%3D6%26searchoption%5B3086%5D%5Btype%5D%3Dradio%26searchoption%5B3046%5D%5Bvalue%5D%3D5%26searchoption%5B3046%5D%5Btype%5D%3Dradio%26sortid%3D311)g
+有一道有关外星人的题目，大致是有很多人声称见到过飞碟和外星人， 有人甚至拍了照片，但有很多照片被证明是自然现象或是飞机卫星， 最后说也有很多其他证据说明外星人的存在。（我选的是最后 一个， 尽管有很多照片被证明是假的，但也不能否定外星人存在的 可能性）
+Case A-信息不足 case B-满足
+               
+
+          
+a和s是夫妻，a喜欢读书，s喜欢看剧，他俩都去健身房，一起回家买菜，吃健康食品。S做饭的时候A就继续看书。然后给了4个选项，问你你能够infer出什么 跟阅读理解题差不多，四个选项都感觉相关，思考了一会不知道就随波选了一个。。
+
+          
+
+          
+
+          
+􀄀􀁦􀆷􀋆􀁖􀄔􀃍􀊇􀀓􀈼􀌄􀃞􀀼􀀥􀈼􀁳􀉬􀀊 􀅩􀋴􀈼􀆷􀀋 􀋬􀉗􀊥􀀡􀄘􀁖􀄔􀇀
+
+
