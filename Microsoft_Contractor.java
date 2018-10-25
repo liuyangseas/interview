8fb57615af7ca93a2aab6368e1a8e1b522cec9e7
zmyvszk <zhoumingyang34@gmail.com>
2018-10-24 17:13:46 -0700

    Create Microsoft_Contractor.java

---
+++ Microsoft_Contractor.java
@@ -0,0 +1,65 @@
+contract of Microsoft interview     机票问题   Leetcode
+
+missing link
+given a list of pair of node    1→ 2, 2→ 3,  return 1→ 3
+
+return  the missing link when it is a possible path
+
+    one more example:
+
+given input as 1->2, 1->4,   2->3, 2->4,     4->5
+
+should return:  1->3, 1->5, 2->5
+
+U can assume all the links given direct from smaller number to larger number
+
+
+compile the formula give the answer        Caculator    Leetcode basic question
+“2 3 * 5 +”   imply 2+3*5
+
+“1 4 * 3 6 * +” imply 1*4+3*6
+
+U can use several function:
+
+GetNextToken()
+
+IsNum()
+
+IsOps()
+
+ActOnCntOfNumbers()
+
+one more question:
+
+what kind of error ?
+
+
+question about resume:
+
+
+process big data?   resume
+
+data processor ? in which language?    resume
+
+multiple thread?
+
+abstract method? VS virtual method?    bingo
+
+sql? left join?                bingo
+
+design pattern??             
+
+API? what kind of data use for API ? (Json file)  how to arrange them??? access to the API?
+
+machine learning?
+
+
+behavior quesion
+
+why do you want to leave your current job?             bingo
+
+any difficult problem during your work? (technical)      bingo
+
+
+
+
